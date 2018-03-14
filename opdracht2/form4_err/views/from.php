ea59dbbc7aaecfeaf74b6ea256f788468bf28269
24617 <31615227+24617@users.noreply.github.com>
2018-03-14 11:29:30 +0100

    Add files via upload

---
+++ opdracht2/form4_err/views/from.php
@@ -0,0 +1,16 @@
+<h1>Contactformulier</h1>
+<h3>Alex Bröking - GD1B</h3>
+<hr>
+<pre>
+  <form action="views/response.php" method="post">
+
+    naam: 	 <input type="text" name="name" autofocus>
+
+    email: 	  <input type="email" name="e-mail">
+
+    bericht: 	<input type="text" name="message">
+
+    			    <input type="submit" name='sendContact'>
+  </form>
+</pre>
+<hr>
